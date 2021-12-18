e4a5cdf9f78775947fc98086dc7eae50476ea2f3
dimalepel <dimalepel@mail.ru>
2021-12-18 17:31:49 +0300

    feat: add function body

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+
+	if (matrix) {
+    function pushDigit(arr) {
+      arr.forEach((item) => {
+        result.push(item);
+      });
+    }
+
+    matrix.forEach((item, index) => {
+      if (index % 2 !== 0) {
+        pushDigit(item.sort().reverse());
+      } else {
+        pushDigit(item);
+      }
+    });
+  }  
+  
+  return result;
 }
